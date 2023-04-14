33e501f3fc92844284cc56d869f416b7f23cd286
Jinga <info.alpain@gmail.com>
2023-04-15 08:52:12 +0900

    <add> initial commit

--- 00_kokudochiriin_dem/src/App.tsx
+++ 00_kokudochiriin_dem/src/App.tsx
@@ -1,30 +1,9 @@
 // 地図表示領域(MapLibre)
 import React from "react";
 import maplibregl from "maplibre-gl";
+import { useGsiTerrainSource } from "maplibre-gl-gsi-terrain";
 import "maplibre-gl/dist/maplibre-gl.css";
 import "./App.css";
-
-// 国土地理院のDEMをTerrainのRGB値に変換する関数
-const gsiDEM2TerrainRGB = (r: number, g: number, b: number): number[] => {
-  let height = r * 655.36 + g * 2.56 + b * 0.01;
-  if (r === 128 && g === 0 && b === 0) {
-    height = 0;
-  } else if (r >= 128) {
-    height -= 167772.16;
-  }
-  height += 100000;
-  height *= 10;
-  const tB = (height / 256 - Math.floor(height / 256)) * 256;
-  const tG =
-    (Math.floor(height / 256) / 256 -
-      Math.floor(Math.floor(height / 256) / 256)) *
-    256;
-  const tR =
-    (Math.floor(Math.floor(height / 256) / 256) / 256 -
-      Math.floor(Math.floor(Math.floor(height / 256) / 256) / 256)) *
-    256;
-  return [tR, tG, tB];
-};
 
 // MapLibreのMapオブジェクト
 let map: maplibregl.Map;
@@ -36,9 +15,33 @@ export default function App() {
     if (!map) {
       if (!mapContainer.current) return;
       // 地図の作成
+      const gsiTerrainSource = useGsiTerrainSource(maplibregl.addProtocol);
       map = new maplibregl.Map({
         container: mapContainer.current,
-        style: "./kokudochiriin_wmts_ortho.json",
+        style: {
+          version: 8,
+          sources: {
+            gsi: {
+              type: "raster",
+              tiles: [
+                "https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg",
+              ],
+              attribution: "地理院タイル",
+            },
+            terrain: gsiTerrainSource,
+          },
+          layers: [
+            {
+              id: "gsi",
+              type: "raster",
+              source: "gsi",
+            },
+          ],
+          terrain: {
+            source: "terrain",
+            exaggeration: 1.2,
+          },
+        },
         center: [138.73, 35.355], // 初期緯度経度
         zoom: 13, // 初期ズーム値
         minZoom: 4, // 最小ズーム値
@@ -49,43 +52,6 @@ export default function App() {
       });
       map.addControl(new maplibregl.NavigationControl({}), "top-right"); // ズーム・回転コントロールの表示
       map.addControl(new maplibregl.ScaleControl({}), "bottom-left"); // スケール値の表示
-      // 国土地理院DEMをTerrainに変換して追加
-      maplibregl.addProtocol("gsidem", (params, callback) => {
-        const image = new Image();
-        image.crossOrigin = "";
-        image.onload = () => {
-          const canvas = document.createElement("canvas");
-          canvas.width = image.width;
-          canvas.height = image.height;
-          const context = canvas.getContext("2d");
-          if (!context) return;
-          context.drawImage(image, 0, 0);
-          const imageData = context.getImageData(
-            0,
-            0,
-            canvas.width,
-            canvas.height
-          );
-          for (let i = 0; i < imageData.data.length / 4; i++) {
-            const tRGB = gsiDEM2TerrainRGB(
-              imageData.data[i * 4],
-              imageData.data[i * 4 + 1],
-              imageData.data[i * 4 + 2]
-            );
-            imageData.data[i * 4] = tRGB[0];
-            imageData.data[i * 4 + 1] = tRGB[1];
-            imageData.data[i * 4 + 2] = tRGB[2];
-          }
-          context.putImageData(imageData, 0, 0);
-          canvas.toBlob((blob) => {
-            if (blob)
-              blob.arrayBuffer().then((arr) => callback(null, arr, null, null));
-            // ここで返すデータは、画像のArrayBuffer()でなければならない
-          });
-        };
-        image.src = params.url.replace("gsidem://", "");
-        return { cancel: () => {} };
-      });
     }
   });
 
